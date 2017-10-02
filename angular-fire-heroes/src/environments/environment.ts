b6ed5c906aba645f1f83c8cddf61432416c64ac6
Luis Pujols <luispujols@hotmail.com>
2017-10-02 12:00:00 -0400

    ADD "Installed and implemented the configuration to support firebase in the app, and also implemented the angular fire 2 modules"

--- angular-fire-heroes/src/environments/environment.ts
+++ angular-fire-heroes/src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCnIHShcvPqpJBlSxv5D8nUR9E3KwJpqzc',
+    authDomain: 'angular-fire-heroes.firebaseapp.com',
+    databaseURL: 'https://angular-fire-heroes.firebaseio.com',
+    projectId: 'angular-fire-heroes',
+    storageBucket: 'angular-fire-heroes.appspot.com',
+    messagingSenderId: '605290368490'
+  }
 };
