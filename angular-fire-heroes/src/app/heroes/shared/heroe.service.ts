bcfb196bc4808f16cd177d5b9e35fc468ce9ee59
Luis Pujols <luispujols@hotmail.com>
2017-10-02 13:51:00 -0400

    ADD "Implemented the CRUD methods for the heroe service, and now the service is completed"

--- angular-fire-heroes/src/app/heroes/shared/heroe.service.ts
+++ angular-fire-heroes/src/app/heroes/shared/heroe.service.ts
@@ -23,7 +23,40 @@ export class HeroeService {
 
     const heroePath =  `${this.basePath}/${key}`;
     this.heroe = this.db.object(heroePath);
+
+    // Print out the key of the heroe
+    let heroeSnapshot;
+    heroeSnapshot = this.db.object(heroePath, { preserveSnapshot: true });
+    heroeSnapshot.subscribe(snapshot => {
+      console.log(snapshot.key);
+      console.log(snapshot.val());
+    });
+
     return this.heroe;
   }
 
+  createHeroe(heroe: Heroe): void {
+    this.heroes.push(heroe)
+      .catch(error => this.handleError(error));
+  }
+
+  updateHeroe(key: string, value: any): void {
+    this.heroes.update(key, value)
+      .catch(error => this.handleError(error));
+  }
+
+  deleteHeroe(key: string): void {
+    this.heroes.remove(key)
+      .catch(error => this.handleError(error));
+  }
+
+  deleteAll(): void {
+    this.heroes.remove()
+      .catch(error => this.handleError(error));
+
+  }
+
+  private handleError(error: Error) {
+    console.log(error);
+  }
 }
